6317df697b89f9f0c797561e17e9a80d12fcb9f0
josepingp <josepingp@gmail.com>
2024-09-08 17:50:31 +0200

    feat: :art: add blade directives to service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,32 @@
 
 namespace App\Providers;
 
+use App\Models\Business;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 */
+	public function register(): void
+	{
+		Blade::if('dateNotIsToday', function (Carbon $date) {
+			return !$date->isToday();
+		});
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+		Blade::if('dateWithinMaxFutureDays', function (Carbon $date, Business $business) {
+			return $date->dayOfYear < (now()->dayOfYear + $business->max_future_days);
+		});
+	}
+
+	/**
+	 * Bootstrap any application services.
+	 */
+	public function boot(): void
+	{
+		//
+	}
 }
