1fe746c176e1b7a489127e79f31496f9bef9e74a
josepingp <josepingp@gmail.com>
2024-09-11 16:33:25 +0200

    feat: :art: add nav links and credits badge to navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -32,6 +32,7 @@
                             class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
 
+
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
                                     viewBox="0 0 20 20">
@@ -39,7 +40,14 @@
                                         d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
                                         clip-rule="evenodd" />
                                 </svg>
+
                             </div>
+
+                            <span
+                                class="ms-2 bg-green-100 dark:bg-green-900 text-green-300 px-2 py-1 rounded-full text-xs font-semibold">
+                                <span>Créditos: {{ Auth::user()->credit }}</span>
+                            </span>
+
                         </button>
                     </x-slot>
 
