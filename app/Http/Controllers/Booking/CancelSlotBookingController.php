23e1ff59b6cae1d40bff8f96b804d660ed50195f
josepingp <josepingp@gmail.com>
2024-09-17 22:11:37 +0200

    add logic to cancel book

--- app/Http/Controllers/Booking/CancelSlotBookingController.php
+++ app/Http/Controllers/Booking/CancelSlotBookingController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Booking;
 
 use App\Http\Controllers\Controller;
+use App\Models\Booking;
+use App\Models\Business;
 use Illuminate\Http\Request;
 
 class CancelSlotBookingController extends Controller
@@ -10,8 +12,17 @@ class CancelSlotBookingController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request,Business $business,Booking $booking)
     {
-        //
+        $booking->delete();
+
+		$request->user()->increment('credit');
+
+		return redirect()->route('slots.show', [
+			'business' => $business,
+			'year' => $booking->slot->slot_date->year,
+			'month' => $booking->slot->slot_date->month,
+			'day' => $booking->slot->slot_date->day
+		]);
     }
 }
